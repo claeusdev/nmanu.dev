ebd7ce3a413f156972cf3789a7ec13faf839193d
Nana Adjei Manu <claeusdev@gmail.com>
2023-08-27 22:17:42 +0200

    chore: add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="robots" content="follow, index" />
         <meta name="description" content={meta.description} />
         <meta property="og:site_name" content={meta.title} />
