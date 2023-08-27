03e3dffb23c796e4dc60f98ef03f8281106d55fb
Nana Adjei Manu <claeusdev@gmail.com>
2023-08-27 22:12:45 +0200

    edit frontmatter

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: "Nextra Blog",
-    description: "Clone and deploy your own Next.js-powered portfolio in minutes.",
-    image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
+    title: "Nana Adjei Manu",
+    description: "Nana's blog",
+    image: "https://avatars.githubusercontent.com/u/17507528?",
   };
 
   return (
@@ -17,7 +17,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@nmanu__" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
