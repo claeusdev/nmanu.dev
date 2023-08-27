80f6fbc21c206556fc1a81b528e0220c420b6743
Nana Adjei Manu <claeusdev@gmail.com>
2023-08-27 21:47:26 +0200

    init commit with first blog

--- pages/posts/index.md
+++ pages/posts/index.md
@@ -1,6 +1,6 @@
 ---
 type: posts
-title: Posts
+title: Thoughts
 date: 2021-03-18
 ---
 
--- theme.config.js
+++ theme.config.js
@@ -3,8 +3,7 @@ const YEAR = new Date().getFullYear();
 export default {
   footer: (
     <small style={{ display: "block", marginTop: "8rem" }}>
-      <time>{YEAR}</time> © Create Nextra App.
-      <a href="/feed.xml">RSS</a>
+      <time>{YEAR}</time> © Nana Adjei Manu.
       <style jsx>{`
         a {
           float: right;
